f27d5aeeec1d9856e1ea721e7aa4a833d17f8c58
mdcruz <marie.descruz@gmail.com>
2025-03-19 12:09:42 +0000

    initial commit

---
+++ config.h
@@ -0,0 +1,11 @@
+// Prometheus details
+#define GC_PROM_URL "prometheus-prod-13-prod-us-east-0.grafana.net"
+#define GC_PROM_USER ""
+#define GC_PROM_PASS ""
+#define GC_PROM_PATH "/api/prom/push"
+
+#define GC_PORT 443
+
+// Wifi details
+#define WIFI_SSID     ""
+#define WIFI_PASSWORD ""
---
+++ plant-monitoring.ino
@@ -0,0 +1,106 @@
+#include <Arduino.h>
+#include <PromLokiTransport.h>
+#include <PrometheusArduino.h>
+
+#include "config.h"
+#include "certificates.h"
+
+// Define the pin
+int sensorPin = 4;
+
+int sensorValue = 0;
+
+// Prometheus client and transport
+PromLokiTransport transport;
+PromClient client(transport);
+
+// Create a write request
+WriteRequest req(1, 3074);
+
+// Define TimeSeries
+TimeSeries ts(5, "soil_moisture",  "{monitoring_type=\"gabby_plant\",board_type=\"esp32\",room=\"living_room\"}");
+
+int loopCounter = 0;
+
+void setup() {
+  // Start the serial output at 9,600 baud 
+  Serial.begin(9600);
+
+  uint8_t serialTimeout;
+  while (!Serial && serialTimeout < 50) {
+      delay(100);
+      serialTimeout++;
+  }
+  
+  Serial.println("Starting");
+  Serial.print("Free Mem Before Setup: ");
+  Serial.println(freeMemory());
+
+  // Configure and start the transport layer
+  transport.setUseTls(true);
+  transport.setCerts(grafanaCert, strlen(grafanaCert));
+  transport.setWifiSsid(WIFI_SSID);
+  transport.setWifiPass(WIFI_PASSWORD);
+  transport.setDebug(Serial);  // Remove this line to disable debug logging of the client.
+  if (!transport.begin()) {
+    Serial.println(transport.errmsg);
+    while (true) {};
+  }
+
+  // Configure the client
+  client.setUrl(GC_PROM_URL);
+  client.setPath((char*)GC_PROM_PATH);
+  client.setPort(GC_PORT);
+  client.setUser(GC_PROM_USER);
+  client.setPass(GC_PROM_PASS);
+  client.setDebug(Serial);  // Remove this line to disable debug logging of the client.
+  if (!client.begin()) {
+    Serial.println(client.errmsg);
+    while (true) {};
+  }
+
+  // Add our TimeSeries to the WriteRequest
+  req.addTimeSeries(ts);
+  req.setDebug(Serial);  // Remove this line to disable debug logging of the write request serialization and compression.
+
+  Serial.print("Free Mem After Setup: ");
+  Serial.println(freeMemory());
+}
+
+void loop() {
+  // Get current timestamp
+  int64_t time;
+  time = transport.getTimeMillis();
+
+  // Read the analog value from the sensor
+  sensorValue = analogRead(sensorPin);
+  
+  // Check if the soil is dry
+  if (sensorValue > 500) {
+    Serial.print(sensorValue);
+    Serial.println(" - Status: Soil is too dry - time to water!");
+  } else {
+     Serial.print(sensorValue);
+    Serial.println(" - Status: Soil is perfect!");
+  }
+
+  if (!ts.addSample(time, sensorValue)) {
+      Serial.println(ts.errmsg);
+  }
+
+  loopCounter++;
+
+  if (loopCounter >= 4) {
+    // Send data
+    loopCounter = 0;
+    PromClient::SendResult res = client.send(req);
+    if (!res == PromClient::SendResult::SUCCESS) {
+        Serial.println(client.errmsg);
+    }
+
+    // Reset batches after a succesful send.
+    ts.resetSamples();
+}
+  // Wait before taking another reading
+  delay(1000);
+}
